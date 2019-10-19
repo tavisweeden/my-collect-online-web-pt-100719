a48f65ccc67ac1416a8d1a9a243377f91ebac1e0
Tavis Weeden <tavisweeden@gmail.com>
2019-10-19 21:34:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,12 @@
 require 'pry'
+
 def collectmeth(array)
   ind = 0
   collection = []
   while ind < array.length
   collection << yield(array[ind])
   ind += 1
-end
 binding.pry
+end
 collection
 end
